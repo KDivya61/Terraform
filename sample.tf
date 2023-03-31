e56690a7ef01a28955da6ef2fc047a6ebdda7441
KDivya61 <Divyak2857646@gmail.com>
2023-03-31 12:24:36 +0000

    c

--- sample.tf
+++ sample.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "ec2" {
 
-    ami ="ami-0089b8e98cd95257d"
-    instance_type="t3.micro"
-    vpc_security_group_ids=["sg-0b1f30eed79978a4f"]
+    ami = "ami-0089b8e98cd95257d"
+    instance_type = "t3.micro"
+    vpc_security_group_ids = ["sg-0b1f30eed79978a4f"]
     tags = {
-        name="test"
+        name = "test"
     }
 }
 # changed the instance to new workstation.
